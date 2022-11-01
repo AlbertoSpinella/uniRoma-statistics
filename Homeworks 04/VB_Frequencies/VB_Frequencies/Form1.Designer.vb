56b0f9918f1f6495f40986ebfe94f82dcec5026e
AlbertoSpinella <alberto.spinella@protonmail.com>
2022-11-01 12:18:31 +0100

    Partial homeworks 04

---
+++ Homeworks 04/VB_Frequencies/VB_Frequencies/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(184, 62)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 123)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(828, 499)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(253, 12)
+        Me.TrackBar1.Maximum = 200
+        Me.TrackBar1.Minimum = 100
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar1.TabIndex = 5
+        Me.TrackBar1.Tag = ""
+        Me.TrackBar1.Value = 100
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(517, 12)
+        Me.TrackBar2.Maximum = 100
+        Me.TrackBar2.Minimum = 1
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar2.TabIndex = 6
+        Me.TrackBar2.Value = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Location = New System.Drawing.Point(257, 53)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 16)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "TrialsCount: 100"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Location = New System.Drawing.Point(517, 53)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(140, 16)
+        Me.TextBox2.TabIndex = 8
+        Me.TextBox2.Text = "ExperimentsCount: 1"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox3.Location = New System.Drawing.Point(773, 53)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(140, 16)
+        Me.TextBox3.TabIndex = 10
+        Me.TextBox3.Text = "SuccessChance: 0.5"
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(773, 12)
+        Me.TrackBar3.Maximum = 100
+        Me.TrackBar3.Minimum = 1
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar3.TabIndex = 9
+        Me.TrackBar3.Value = 50
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(846, 123)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(238, 499)
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1096, 634)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TrackBar3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TrackBar2)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ Homeworks 04/VB_Frequencies/VB_Frequencies/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.Net.Sockets
+
+Public Class Form1
+
+    Public b As Bitmap
+    Public g As Graphics
+    Public r As New Random
+
+    Sub InitializeGraphics()
+        Me.b = New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+        Me.g = Graphics.FromImage(b)
+        Me.g.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+        Me.g.TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAlias
+    End Sub
+
+    Function GenerateListOfPoints(VirtualWindow As Rectangle) As Tuple(Of List(Of Point), List(Of Point))
+
+        Dim TrialsCount As Integer = Me.TrackBar1.Value
+        Dim SuccessChance As Double = Me.TrackBar3.Value / 100
+
+        Dim MinX As Double = 0
+        Dim MaxX As Double = TrialsCount
+        Dim MinY As Double = 0
+        Dim MaxY As Double = TrialsCount
+
+        Dim PointsOfAbsoluteFrequency As New List(Of Point)
+        Dim PointsOfRelativeFrequency As New List(Of Point)
+
+        Dim Y As Double = 0
+        For X As Integer = 1 To TrialsCount
+            Dim RandomNumber As Double = r.NextDouble
+            If RandomNumber < SuccessChance Then
+                Y = Y + 1
+            End If
+            Dim X_Device_Absolute As Integer = FromXWorldToXVirtual(X, MinX, MaxX, VirtualWindow.Left, VirtualWindow.Width)
+            Dim Y_Device_Absolute As Integer = FromYWorldToYVirtual(Y, MinY, MaxY, VirtualWindow.Top, VirtualWindow.Height)
+            PointsOfAbsoluteFrequency.Add(New Point(X_Device_Absolute, Y_Device_Absolute))
+
+            Dim X_Device_Relative As Integer = FromXWorldToXVirtual(X, MinX, MaxX, VirtualWindow.Left, VirtualWindow.Width)
+            Dim Y_Device_Relative As Integer = FromYWorldToYVirtual_Relative(Y / X, VirtualWindow.Top, VirtualWindow.Height)
+            PointsOfRelativeFrequency.Add(New Point(X_Device_Relative, Y_Device_Relative))
+
+        Next
+        Return New Tuple(Of List(Of Point), List(Of Point))(PointsOfAbsoluteFrequency, PointsOfRelativeFrequency)
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.InitializeGraphics()
+
+        Dim VirtualWindow As New Rectangle(25, 25, Me.b.Width - 50, Me.b.Height - 50)
+
+        g.DrawRectangle(Pens.Black, VirtualWindow)
+
+        Dim ExperimentsCount As Integer = Me.TrackBar2.Value
+
+        For Experiment As Integer = 1 To ExperimentsCount
+            Dim AllPoints As Tuple(Of List(Of Point), List(Of Point)) = Me.GenerateListOfPoints(VirtualWindow)
+
+
+            g.DrawLines(Pens.Red, AllPoints.Item1.ToArray)
+            g.DrawLines(Pens.Green, AllPoints.Item2.ToArray)
+        Next
+
+        Me.PictureBox1.Image = b
+    End Sub
+
+    Function FromXWorldToXVirtual(X As Double, MinX As Double, MaxX As Double, Left As Integer, W As Integer) As Integer
+
+        If (MaxX - MinX) = 0 Then Return 0
+        Return Left + W * (X - MinX) / (MaxX - MinX)
+
+    End Function
+
+    Function FromYWorldToYVirtual(Y As Double, MinY As Double, MaxY As Double, Top As Integer, H As Integer) As Integer
+
+        If (MaxY - MinY) = 0 Then Return 0
+        Return Top + H - H * (Y - MinY) / (MaxY - MinY)
+
+    End Function
+
+    Function FromYWorldToYVirtual_Relative(Y As Double, Top As Integer, H As Integer) As Integer
+
+        Return Top + H - H * Y
+
+    End Function
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        Me.TextBox1.Text = "TrialsCount: " & Me.TrackBar1.Value
+    End Sub
+
+    Private Sub TrackBar2_Scroll(sender As Object, e As EventArgs) Handles TrackBar2.Scroll
+        Me.TextBox2.Text = "ExperimentsCount: " & Me.TrackBar2.Value
+    End Sub
+
+    Private Sub TrackBar3_Scroll(sender As Object, e As EventArgs) Handles TrackBar3.Scroll
+        Me.TextBox3.Text = "SuccessChance: " & Me.TrackBar3.Value / 100
+    End Sub
+End Class
